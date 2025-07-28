310663dbb4d8230b37d6e04f75e9cc422dd12b82
borislavkokeza <borislav.kokeza@planetsoft.eu>
2025-07-28 07:52:31 +0200

    vite config update

--- budget/vite.config.ts
+++ budget/vite.config.ts
@@ -1,8 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const env = loadEnv('', process.cwd());
+const url = env.VITE_API_URL;
+const port = env.VITE_API_PORT;
+const apiUrl = `${url}:${port}/api`;
+
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -11,5 +15,14 @@ export default defineConfig({
     setupFiles: ['./test/setup.ts'],
     css: true,
     exclude: ['**/node_modules/**', '**/test/pact-tests/**', '**/test/playwright-tests/**'],
+  },server: {
+    proxy: {
+      '/api': {
+        target: apiUrl,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
